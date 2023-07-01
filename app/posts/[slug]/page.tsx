a167e419bbb5201f68783d54d698c16a60c9c225
Hanad Mohamed <hanaddaahir124@gmail.com>
2023-07-01 14:21:06 +0300

    fixing build time bugs

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -13,12 +13,13 @@ const getPostContent = (slug: string) => {
   return matterResult;
 };
 
-const generateStaticParam = async () => {
+export const generateStaticParams = async () => {
   const posts = getPostMetadata();
-  return posts.map((post) => {
-    slug: post.slug;
-  });
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
 };
+
 const PostPage = (props: any) => {
   const slug = props.params.slug;
   const post = getPostContent(slug);
